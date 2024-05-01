171ac6cbf44e69163ef82f8d695244ce26715302
CHINMOY-DEY-CINI <chinmoydey942728@gmail.com>
2024-05-01 10:49:15 +0600

    artcra

---
+++ src/allcomponents/artcra.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Artdetails() {
+    
+    return (
+        <div className="text-center py-12 px-6">
+            <h3 className="text-4xl font-bold mb-4">Art & Craft Company</h3>
+            <p>At The Art & Craft Company we have almost 30 years of experience in supplying quality brands fine art materials, craft supplies, hobby & leisure and educational materials.  Our customers include schools, creches, playschools, teachers and students, graphic artists, graffiti artists, beginners, improvers and professional artists, hobbyists, home and interior decor enthusiasts and crafters.  We stock top brands in Fine Art Paints, Art Papers and Craft Card, Sketchbooks and drawing paper, Modelling Clays, Adhesives, Acrylic Paints, Oil Paints, Watercolours, Fine Art Brushes, Canvasses, Graphics and Drawing pens and markers, FIMO Polymer clay, Spray Paints, Face Paints and much more.</p>
+        </div>
+    )
+}
+
+
+
+export default Artdetails
